f692e3fd5939faa3eb767462a5d83367bc5b3e2b
John Moon <john.moon@livingsocial.com>
2015-12-08 00:58:48 -0500

    Remove dead code
    Remove most options

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,12 @@ class HomeController < ApplicationController
   SEATGEEK_BASE_URL = 'http://api.seatgeek.com/2/taxonomies'
   def index
     @month_of_the_year = month_of_the_year
-    @number_of_tickets = number_of_tickets
     @state = state
     @listing_count = listing_count
     @event_type = event_type
   end
 
   private
-
-  def number_of_tickets
-    [[1],[2],[3]]
-  end
 
   def month_of_the_year
     @month_of_the_year = [{name: 'Jan 2016', value: '2016-01'},
@@ -30,19 +25,20 @@ class HomeController < ApplicationController
   end
 
   def state
-    @state = [{:state_name=>"AL"}, {:state_name=>"MT"}, {:state_name=>"AK"}, {:state_name=>"NE"},
-              {:state_name=>"AZ"}, {:state_name=>"NV"}, {:state_name=>"AR"}, {:state_name=>"NH"},
-              {:state_name=>"CA"}, {:state_name=>"NJ"}, {:state_name=>"CO"}, {:state_name=>"NM"},
-              {:state_name=>"CT"}, {:state_name=>"NY"}, {:state_name=>"DE"}, {:state_name=>"NC"},
-              {:state_name=>"FL"}, {:state_name=>"ND"}, {:state_name=>"GA"}, {:state_name=>"OH"},
-              {:state_name=>"HI"}, {:state_name=>"OK"}, {:state_name=>"ID"}, {:state_name=>"OR"},
-              {:state_name=>"IL"}, {:state_name=>"PA"}, {:state_name=>"IN"}, {:state_name=>"RI"},
-              {:state_name=>"IA"}, {:state_name=>"SC"}, {:state_name=>"KS"}, {:state_name=>"SD"},
-              {:state_name=>"KY"}, {:state_name=>"TN"}, {:state_name=>"LA"}, {:state_name=>"TX"},
-              {:state_name=>"ME"}, {:state_name=>"UT"}, {:state_name=>"MD"}, {:state_name=>"VT"},
-              {:state_name=>"MA"}, {:state_name=>"VA"}, {:state_name=>"MI"}, {:state_name=>"WA"},
-              {:state_name=>"MN"}, {:state_name=>"WV"}, {:state_name=>"MS"}, {:state_name=>"WI"},
-              {:state_name=>"MO"}, {:state_name=>"WY"}].sort_by { |x| x[:state_name] }
+    # @state = [{:state_name=>"AL"}, {:state_name=>"MT"}, {:state_name=>"AK"}, {:state_name=>"NE"},
+    #           {:state_name=>"AZ"}, {:state_name=>"NV"}, {:state_name=>"AR"}, {:state_name=>"NH"},
+    #           {:state_name=>"CA"}, {:state_name=>"NJ"}, {:state_name=>"CO"}, {:state_name=>"NM"},
+    #           {:state_name=>"CT"}, {:state_name=>"NY"}, {:state_name=>"DE"}, {:state_name=>"NC"},
+    #           {:state_name=>"FL"}, {:state_name=>"ND"}, {:state_name=>"GA"}, {:state_name=>"OH"},
+    #           {:state_name=>"HI"}, {:state_name=>"OK"}, {:state_name=>"ID"}, {:state_name=>"OR"},
+    #           {:state_name=>"IL"}, {:state_name=>"PA"}, {:state_name=>"IN"}, {:state_name=>"RI"},
+    #           {:state_name=>"IA"}, {:state_name=>"SC"}, {:state_name=>"KS"}, {:state_name=>"SD"},
+    #           {:state_name=>"KY"}, {:state_name=>"TN"}, {:state_name=>"LA"}, {:state_name=>"TX"},
+    #           {:state_name=>"ME"}, {:state_name=>"UT"}, {:state_name=>"MD"}, {:state_name=>"VT"},
+    #           {:state_name=>"MA"}, {:state_name=>"VA"}, {:state_name=>"MI"}, {:state_name=>"WA"},
+    #           {:state_name=>"MN"}, {:state_name=>"WV"}, {:state_name=>"MS"}, {:state_name=>"WI"},
+    #           {:state_name=>"MO"}, {:state_name=>"WY"}].sort_by { |x| x[:state_name] }
+    [{:state_name=>"NY"}, {:state_name=>"CA"}, {:state_name=>"PA"}, {:state_name=>"FL"}]
   end
 
   def listing_count
